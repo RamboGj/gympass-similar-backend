0e7597c6abe5a70f26cce20e862bb2236decde1d
artemis <jpramboguanabara@hotmail.com>
2026-05-12 17:00:17 -0300

    feat: database entities + controller

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,11 +4,14 @@ import { z } from "zod"
 const envSchema = z.object({
     NODE_ENV: z.enum(["dev", "test", "prod"]).default("dev"),
     PORT: z.coerce.number().default(3333),
+    DATABASE_URL: z.url()
+
 })
 
 const _env = envSchema.safeParse({
     NODE_ENV: process.env["NODE_ENV"],
     PORT: process.env["PORT"],
+    DATABASE_URL: process.env["DATABASE_URL"]
 })
 
 if (!_env.success) {
